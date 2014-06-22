fcb007abccc3126014cc18d66f79098f1fed5eaa
belenbgh <belen.bgh@gmail.com>
2014-06-22 17:43:02 +0200

    Course project script and text file

--- run_analysis.R
+++ run_analysis.R
@@ -9,26 +9,13 @@
 
 ## Read common information for both test and train data sets:
 
-activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt",
+	activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt",
+						colClasses = "character",
+						col.names = c("activity_id", "activity_desc"))
+
+	features <- read.table("UCI HAR Dataset/features.txt",
 					colClasses = "character",
-					col.names = c("activity_id", "activity_desc"))
-
-features <- read.table("UCI HAR Dataset/features.txt",
-				colClasses = "character",
-				col.names = c("X_col_number", "feature_desc"))
-
-# Esto de abajo aun no se si lo quiero hacer aqui...
-
-## We want to remove the characters "-", "(", ")" and "," in order to have
-## valid column names, but preserving the original column
-
-#ilegal_chars <- c("-", "[:(:]", "[:):]", ",")
-
-features$feature_desc_2 <- gsub(pattern = "-", replacement = "_", features$feature_desc)
-features$feature_desc_2 <- gsub(pattern = "[:(:]", replacement = "_", features$feature_desc_2)
-features$feature_desc_2 <- gsub(pattern = "[:):]", replacement = "_", features$feature_desc_2)
-features$feature_desc_2 <- gsub(pattern = ",", replacement = "_", features$feature_desc_2)
-
+					col.names = c("X_col_number", "feature_desc"))
 
 ## Read test data:
 
@@ -47,7 +34,7 @@ features$feature_desc_2 <- gsub(pattern = ",", replacement = "_", features$featu
 
 	test_data <- cbind(subject_test, y_test, X_test)
 	
-	# Finally, we add colum names to X_test: 
+	# Finally, we add descriptive colum names to X_test: 
 	# subject_id, activity, feature_1, feature_2, ..., feature_561
 
 	names(test_data) <- c("subject_id", "activity_id", features$feature_desc)
@@ -73,86 +60,82 @@ features$feature_desc_2 <- gsub(pattern = ",", replacement = "_", features$featu
 
 	train_data <- cbind(subject_train, y_train, X_train)
 		
-	# Finally, we add colum names to X_test: 
-	# subject_id, activity, feature_1, feature_2, ..., feature_561
+	# Finally, we add descriptive colum names to X_test: 
+	# subject_id, activity_id, feature_1, feature_2, ..., feature_561
 
 	names(train_data) <- c("subject_id", "activity_id", features$feature_desc)
 
-## Put together test and train data: (puesto que aqui - si no lo estoy
-## entendiendo mal - solo hay que poner un conjunto a continuacion del otro, quiza 
-## deberías hacerlo antes de haber estado haciendo paso por duplicado... no?
-## me refiero a convertir los activity labels y eso...)
 
-data <- rbind(test_data, train_data)
+## Put together test and train data: 
 
+	raw_data <- rbind(test_data, train_data)
 
-## Since we are only insterested in those features involving mean and std
-## of the measurements, we'll extract those columns of the X_test and X_train
-## data sets...
-
-# returns the indexes of the rows where the pattern occurs
-mean_columns <- grep(pattern = "mean", features$feature_desc)
-std_columns <- grep(pattern = "std", features$feature_desc)
-# Combine the former two to obtain one vector with the row indexes we are 
-# interested in (which will turn to be column indexes to X data)
-mean_std_columns <- sort(c(mean_columns, std_columns))
-
-# ...ooooooooooooo!!!
-
-mean_std_cols <- grep(pattern = "mean|std", names(data))
-# ojo! con esto se me estan colando las meanFreq, va??
-
-## Y nos quedamos solo con aquellas columnas cuyos indices nos ha dado la operacion
-## anterior, mas las columnas subject and activity
-data_2 <- data[, c(1,2,mean_std_cols)]
+## At this point, we have a data frame in which each row correspond for all the
+## features (1 to 563) for a subject (1 to 30) and for an activity (1 to 6);
+## 10299 obs. of 563 variables (561 features + subject_id + activity_id)
 
 
-# We use activityLabel to transform to number labels to more
-	# descriptive labels. Once we have put together all the data refering the test
-	# set, we can afford merging with the activityLabels data frame (it couldn't be
-	# done before because the order relation betewn the three previos data sets)
+## Of all features, we are only interested in those involving mean and std
+## of the measurements, so we'll keep just those columns of raw_data:
 
-	data <- merge(data, 
-			 activity_labels, 
-			 by.x = "activity_id",
-			 by.y = "activity_id",
-			 all = TRUE)
-	# se me esta quejando de algunas columnas y no se si es por los caracteres 
-	# ilegales en los nombres de las mismas... Nada, creo q se queja porque los
-	# nombres de las columnas son demasiado largos. 
-	# La cosa es ques esto reordena las columnas (además de las filas) asi que
-	# deberíamos juntar los sets test y train antes de hacer el merge con los
-	# activity_labels. Pero... tiene que haber una forma de hacerlo en plan
-	# operaciones vectorizas!! no??
-	# Bueno, en cualquier caso, lo hagas como lo hagas, no tiene sentido hacerlo
-	# dos veces, asi que lo llevo para abajo, ok??
+	# Returns the indexes of the column names where the pattern occurs:
+	mean_std_cols <- grep(pattern = "mean\\(\\)|std\\(\\)", names(raw_data))
+
+	# Select those columns from raw_data (in addition to subject and activity
+	# columns):
+	raw_data_2 <- raw_data[ , c(1, 2, mean_std_cols)]
+
+## We now have a data frame in which each row correspond for the mean and std
+## features (1 to 66) for a subject (1 to 30) and for an activity (1 to 6);
+## 10299 obs. of 68 variables (66 features + subject_id + activity_id)
 
 
+## We use activityLabel to transform from number labels to more
+## descriptive labels. Once we have put together all the data refering the test
+## set, we can afford merging with the activityLabels data frame (it couldn't be
+## done before because the order relation betewn the three previos data sets)
+
+	raw_data_2$activity_id <- as.factor(raw_data_2$activity_id)
+
+	levels(raw_data_2$activity_id) <- activity_labels$activity_desc
+
+	# And change the name of the column to better fit its content
+	names(raw_data_2)[2] <- "activity_desc"
 
 
 ## Compute the average of each variable for each subject and each activity
 ## and store it in a new data frame:
-## [quizá es interesante en este punto, crearme una clave unica que junte 
-## individuo y actividad, aunq en realidad no se qué me aporta esto o si 
-## merece la pena...]
- 
 
+	# Assuming 'reshape2' is installed, load the package
+	library(reshape2)
 
+	# First, melt the raw_data data frame
+	
+	raw_data_cols <- names(raw_data_2)
+	melted_data <- melt(raw_data_2, 
+				  id = c("subject_id", "activity_desc"), 
+				  measure.vars = raw_data_cols[3:length(raw_data_cols)])
 
+	# Then, agregate it computing the mean for each variable for each 
+	# combination of subject_id and activity_desc
+	
+	tidy_data <- dcast(melted_data, subject_id + activity_desc ~ variable, mean)
 
+## So far, we've not had to adressed any column by dataframe$col_name, but in 
+## order to avoid problems with the column names in future analysis of this data 
+## set, let's make them R legal column names:
 
+	#(aunque esto ahora va a quedar mucho mas feo en el codebook, no?!)
 
-## what form should the text file be in?
-## Of appropriate formats for this assignment and all the formats that Coursera 
-## will accept, a good intersection of the two is a txt file made with 
-## write.table. I sincerely hope by the end of week 3 anyone marking your work 
-## (and thus who has done the assignment) is capable of dealing with a text 
-## file. Particularly if you make it easy for your markers by explaining how to 
-## read it into R (for instance using read.table). Protip, if using write.table,
-## set the option of not writing the line numbers as that can look a bit ugly 
-## when reading it back in.
+	names(tidy_data) <- make.names(names(tidy_data)) 
+	# pues si... :(
 
-write.table(dataframe, 
-		file = "mean_std_averages_by_activity_and_subject.txt", 
-		row.names = FALSE)
+## Finally, write a text file with the tidy data set; set the option of not 
+## writing the line numbers preventing them to appear in future readings of 
+## the file:
+	
+	write.table(tidy_data, 
+			file = "mean_std_averages_by_activity_and_subject.txt", 
+			row.names = FALSE)
+
 
